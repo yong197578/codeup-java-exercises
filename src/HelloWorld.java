e394930a2293497b528abcd407121cf64576b35c
Yong Choi <yong.s.choi2@gmail.com>
2023-04-14 14:45:04 -0500

    first exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //string must be "" sing quotes are reserved fpr single characters
+        //sout + tab system.out.println
+        System.out.println("Hello World");
+//        System.out.println('d');
+        System.out.println("Hello, Welcome to Java!");
+        System.out.print("Hello My Name is ");
+        System.out.print("Yong!");
+    }
+}
